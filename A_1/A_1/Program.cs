72ea214da5b0b9ab0a13fc6c63ce1e803ef278e0
AHatim <ArifHatim.ZainalAbidin@materialise.com.my>
2017-03-06 17:49:46 +0800

    Add Diamond Shaper

--- A_1/A_1/Program.cs
+++ A_1/A_1/Program.cs
@@ -15,6 +15,43 @@ namespace A_1
             int n = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Value entered is : {0}", n);
 
+            if(n % 2 == 0)
+            {
+                Console.WriteLine("Not a valid number, need an odd number!");
+                return;
+            }
+
+            int mid_index = (int)Math.Ceiling((double)n / 2) - 1;
+
+
+            for(int j = 0; j < n; ++j)
+            {
+                for (int i = 0; i < n; ++i)
+                {
+                    int draw_start_index = 0;
+                    int draw_end_index = 0; 
+
+                    if (j <= mid_index)
+                    {
+                        draw_start_index = mid_index - j;
+                        draw_end_index = mid_index + j;
+                    }
+                    else
+                    {
+                        draw_start_index = j - mid_index;
+                        draw_end_index = (n - 1) - (j - mid_index);
+                    }
+
+                    if (i >= draw_start_index && i <= draw_end_index)
+                        Console.Write("#");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+
+
+
             Console.ReadLine();
         }
     }
